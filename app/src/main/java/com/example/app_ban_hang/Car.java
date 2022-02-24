3db0753f6ddb6156453e7f34501202961a17f322
khoa <hatgiong@gmail.com>
2022-02-25 06:20:14 +0700

    module

--- app/src/main/java/com/example/app_ban_hang/Car.java
+++ app/src/main/java/com/example/app_ban_hang/Car.java
@@ -2,10 +2,13 @@ package com.example.app_ban_hang;
 
 import android.util.Log;
 
+import javax.inject.Inject;
+
 public class Car {
     Engine engine;
     Wheel wheel;
 
+    @Inject
     public Car(Engine engine, Wheel wheel) {
         this.engine = engine;
         this.wheel = wheel;
--- app/src/main/java/com/example/app_ban_hang/Wheel.java
+++ app/src/main/java/com/example/app_ban_hang/Wheel.java
@@ -1,8 +1,11 @@
 package com.example.app_ban_hang;
 
+import javax.inject.Inject;
+
 public class Wheel {
     int quantily;
 
+    @Inject
     public Wheel(int quantily) {
         this.quantily = quantily;
     }
